df7c8f90ceaf1ad4b52146285d0ab0e72ba951fe
Lucas GILHARD <l.gilhard@gmail.com>
2020-03-28 11:25:13 +0100

    add login BLoC

---
+++ lib/blocs/login/login_state.dart
@@ -0,0 +1,104 @@
+import 'package:meta/meta.dart';
+import 'package:equatable/equatable.dart';
+
+class LoginState extends Equatable {
+  final bool isEmailValid;
+  final bool isPasswordValid;
+  final bool isLoading;
+  final bool isSuccess;
+  final bool isFailure;
+
+  List<Object> get props =>
+      [isEmailValid, isPasswordValid, isLoading, isSuccess, isFailure];
+
+  bool get isFormValid => isEmailValid && isPasswordValid;
+
+  LoginState({
+    @required this.isEmailValid,
+    @required this.isPasswordValid,
+    @required this.isLoading,
+    @required this.isSuccess,
+    @required this.isFailure,
+  });
+
+  factory LoginState.empty() {
+    return LoginState(
+      isEmailValid: true,
+      isPasswordValid: true,
+      isLoading: false,
+      isSuccess: false,
+      isFailure: false,
+    );
+  }
+
+  factory LoginState.loading() {
+    return LoginState(
+      isEmailValid: true,
+      isPasswordValid: true,
+      isLoading: true,
+      isSuccess: false,
+      isFailure: false,
+    );
+  }
+
+  factory LoginState.failure() {
+    return LoginState(
+      isEmailValid: true,
+      isPasswordValid: true,
+      isLoading: false,
+      isSuccess: false,
+      isFailure: true,
+    );
+  }
+
+  factory LoginState.success() {
+    return LoginState(
+      isEmailValid: true,
+      isPasswordValid: true,
+      isLoading: false,
+      isSuccess: true,
+      isFailure: false,
+    );
+  }
+
+  LoginState update({
+    bool isEmailValid,
+    bool isPasswordValid,
+  }) {
+    return copyWith(
+      isEmailValid: isEmailValid,
+      isPasswordValid: isPasswordValid,
+      isSubmitting: false,
+      isSuccess: false,
+      isFailure: false,
+    );
+  }
+
+  LoginState copyWith({
+    bool isEmailValid,
+    bool isPasswordValid,
+    bool isSubmitEnabled,
+    bool isSubmitting,
+    bool isSuccess,
+    bool isFailure,
+  }) {
+    return LoginState(
+      isEmailValid: isEmailValid ?? this.isEmailValid,
+      isPasswordValid: isPasswordValid ?? this.isPasswordValid,
+      isLoading: isSubmitting ?? this.isLoading,
+      isSuccess: isSuccess ?? this.isSuccess,
+      isFailure: isFailure ?? this.isFailure,
+    );
+  }
+
+  @override
+  String toString() {
+    return '''LoginState {
+      isEmailValid: $isEmailValid,
+      isPasswordValid: $isPasswordValid,
+      isLoading: $isLoading,
+      isSuccess: $isSuccess,
+      isFailure: $isFailure,
+    }''';
+  }
+}
